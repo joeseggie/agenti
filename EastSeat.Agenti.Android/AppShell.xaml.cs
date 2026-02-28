cee6878209cfa5cb48d3e1794f86c2eddbb5dd9b
Joseph Serunjogi <joeseggie@gmail.com>
2026-02-28 15:27:39 +0300

    Update UX and UI for the Android app

--- EastSeat.Agenti.Android/AppShell.xaml.cs
+++ EastSeat.Agenti.Android/AppShell.xaml.cs
@@ -1,9 +1,25 @@
-﻿namespace EastSeat.Agenti.Android;
+﻿using EastSeat.Agenti.Android.Services;
+using System.Windows.Input;
+
+namespace EastSeat.Agenti.Android;
 
 public partial class AppShell : Shell
 {
-	public AppShell()
-	{
-		InitializeComponent();
-	}
+    private readonly IAuthService _authService;
+
+    public ICommand LogoutCommand { get; }
+
+    public AppShell(IAuthService authService)
+    {
+        InitializeComponent();
+        _authService = authService;
+        LogoutCommand = new Command(async () => await PerformLogoutAsync());
+        BindingContext = this;
+    }
+
+    private async Task PerformLogoutAsync()
+    {
+        await _authService.LogoutAsync();
+        Application.Current!.MainPage = IPlatformApplication.Current!.Services.GetRequiredService<Pages.LoginPage>();
+    }
 }
